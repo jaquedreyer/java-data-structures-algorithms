63f57442af3fa85573efc138412aeca22356cc5f
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-12-02 19:22:55 -0300

    chore: add access modifiers notes

--- src/oop/CarClass.java
+++ src/oop/CarClass.java
@@ -12,8 +12,14 @@ public class CarClass {
     }
 
     // Methods
-    public void speedUp() {
+    protected void speedUp() { // outside the oop package is not accessible
         System.out.println("Acelerando o carro..." + model);
     }
+
+    // NOTES - Access Modifiers
+    // public, private, and protected keyword are access modifiers
+    // public (accessible everywhere) - inside and outside the class and package. It can be assigned to variables, methods, constructors, and classes
+    // private (is accessible only within the same class). It can be assigned to variables, methods, and inner classes
+    // protected (is accessible within the package). However, it can also be accessible outside the package but through inheritance only! It can be assigned to variables, methods, constructors and inner classes
 }
 
