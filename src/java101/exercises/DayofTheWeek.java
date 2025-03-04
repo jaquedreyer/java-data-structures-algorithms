33f0c3e25ff18ab317abb46411585511809ac733
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2025-03-04 10:23:11 -0300

    chore: add control statement exercices

---
+++ src/java101/exercises/DayofTheWeek.java
@@ -0,0 +1,47 @@
+package java101.exercises;
+
+import java.util.Scanner;
+
+public class DayofTheWeek {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o valor de 1 a 7, para descobrir o dia da semana");
+        int x = sc.nextInt();
+
+        String dia;
+        switch (x) {
+            case 1:
+                dia = "domingo";
+                break;
+            case 2:
+                dia = "segunda";
+                break;
+            case 3:
+                dia = "terca";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+            default:
+                dia = "valor invalido";
+                break;
+        }
+
+
+        System.out.println("Dia da semana: " + dia);
+
+        sc.close();
+    }
+}
---
+++ src/java101/exercises/PhoneBill.java
@@ -0,0 +1,24 @@
+package java101.exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class PhoneBill {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int minutos = sc.nextInt();
+
+        double conta = 50.0;
+
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0; // Assignment operators
+        }
+        System.out.printf("Valor da conta = R$ %.2f%n", conta);
+
+        sc.close();
+    }
+}
